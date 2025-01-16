cf1a2a8d4271fadc88c51d2755315fed78bdba9e
William Reed <wreed2020@fau.edu>
2025-01-15 22:40:14 -0500

    Initial Commit

---
+++ WorkoutPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutPlanner
+//
+//  Created by Will Reed on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutPlanner/WorkoutPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutPlannerApp.swift
+//  WorkoutPlanner
+//
+//  Created by Will Reed on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
