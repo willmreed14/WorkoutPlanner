1d1a7760248965b094ff983e514833d0b742d024
William Reed <willmreed14@gmail.com>
2025-01-16 20:17:39 -0500

    Create label in the top left of the screen

--- WorkoutPlanner/ContentView.swift
+++ WorkoutPlanner/ContentView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{ // Vertical stack
+            HStack{ // Horizontal stack
+                Label("Monday: Push", systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
+                Spacer() // Push the content to the left
+            }
+            Spacer() // Push the content to the top
         }
-        .padding()
+        .padding() // Keep it spaced from the screen edges
     }
 }
 
 #Preview {
     ContentView()
 }
+
