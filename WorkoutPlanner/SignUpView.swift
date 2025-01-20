9e80074e360f14b0dfcf22b50d42d02def5c9d27
William Reed <willmreed14@gmail.com>
2025-01-19 22:19:51 -0500

    Add sign up view

--- WorkoutPlanner/SignUpView.swift
+++ WorkoutPlanner/SignUpView.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @StateObject private var viewModel = AuthViewModel()
+    @State private var email: String = ""
+    @State private var password: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Sign Up")
+                .font(.largeTitle)
+                .bold()
+                .padding(.bottom, 20)
+
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .padding()
+
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+
+            Button(action: { viewModel.signUp(email: email, password: password) }) {
+                Text("Sign Up")
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding()
+
+            Spacer()
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     SignUpView()
