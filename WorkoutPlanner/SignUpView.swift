0ef5e620cdfebdd57e320cbdd84139ebe40640aa
William Reed <willmreed14@gmail.com>
2025-01-19 21:31:00 -0500

    Create sign in view

---
+++ WorkoutPlanner/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  WorkoutPlanner
+//
+//  Created by Will Reed on 1/19/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
