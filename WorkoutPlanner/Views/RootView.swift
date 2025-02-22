7391de6b5de67b1eeacbb1f288c32484f997f572
William Reed <willmreed14@gmail.com>
2025-02-21 21:26:50 -0500

    created ProgramsView, to see all programs possessed by the user

---
+++ WorkoutPlanner/Views/RootView.swift
@@ -0,0 +1,31 @@
+//
+//  RootView.swift
+//  WorkoutPlanner
+//
+//  Created by Will Reed on 1/19/25.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct RootView: View {
+    @StateObject private var authViewModel = AuthViewModel()
+    
+    var body: some View {
+        Group { // is user authenticated?
+            if authViewModel.isAuthenticated {
+                HomeView() // redirect to home page if auth'd
+            } else {
+                SignInView() // otherwise, direct to sign-in
+            }
+        }
+        .environmentObject(authViewModel) // pass the authViewModel to child views
+        .onAppear{
+            authViewModel.isAuthenticated = Auth.auth().currentUser != nil
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
